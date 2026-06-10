ec5a330ea9857f896a3a474e8a589ae2be7b85a8
Lust Chain <contact@lust.finance>
2026-06-10 16:45:18 -0300

    Delete lib directory

--- lib/fonts.ts
+++
@@ -1,8 +0,0 @@
-import { Ubuntu } from 'next/font/google'
-
-export const ubuntu = Ubuntu({
-  subsets: ['latin'],
-  weight: ['400', '500', '700'],
-  display: 'swap',
-  variable: '--font-ubuntu',
-})
